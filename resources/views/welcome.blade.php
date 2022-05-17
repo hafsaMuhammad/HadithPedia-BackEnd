56806855d145d235a7fea05ca176d03eaec48a85
Hafsa Muhammad <hafsa.mohamed1999@gmail.com>
2022-05-17 18:45:30 +0200

    added welcome links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome</title>
+    <style>
+        .links{
+            display: flex;
+            justify-content:center;
+        }
+        .links a {
+            text-decoration: none;
+            color: #737373;
+            font-weight: 700;
+            font-size: 25px;
+            margin: 0px 20px;
+            text-shadow: 2px 2px 5px #c1ffe9
+        }
+        .links a:hover{
+            text-shadow: 2px 2px 5px #737373;
+            color: #5a796d;
+        }
+    </style>
 </head>
 <body style="background-color: #FFFFF0">
     <div style="text-align: center; padding-top: 5%">
         <img src="https://i.ibb.co/2kQh3xM/logo2.png" alt="logo">
+    </div>
+    <div class="links">
+     <a href="https://hadith-pedia.herokuapp.com/hadithQuestions/create">إضافة سؤال جديد</a>
+    <a href="https://hadith-pedia.herokuapp.com/hadiths/create">إضافة حديث جديد</a>
     </div>
 </body>
 </html>
