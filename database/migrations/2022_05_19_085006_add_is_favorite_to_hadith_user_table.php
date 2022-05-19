2c24b56e8f3d34a0d0b98a44d15eb0f4ab8e6e7c
Hafsa Muhammad <hafsa.mohamed1999@gmail.com>
2022-05-19 19:58:08 +0200

    edited isfavorite

--- database/migrations/2022_05_19_085006_add_is_favorite_to_hadith_user_table.php
+++ database/migrations/2022_05_19_085006_add_is_favorite_to_hadith_user_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('hadith_user', function (Blueprint $table) {
-            $table->boolean('isFavorite');
+            $table->boolean('isFavorite')->nullable();
         });
     }
 
